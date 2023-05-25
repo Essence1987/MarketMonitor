5fcec79b29d2489d27ab506720574a854280319b
Essence1987 <hwmelander@gmail.com>
2023-05-24 19:42:39 -0700

    waiting for approval from CryptoPanic before modifying Java further.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,24 @@
 var key = config.marketaux;
+
+const authToken = '3491e214e88f50ea6f1b431ad848d7391a9ef924';
+const apiUrl = 'https://cryptopanic.com/api/v1/posts/';
+
+// Function to fetch API data
+async function fetchData() {
+  try {
+    const response = await fetch(`${apiUrl}?auth_token=${authToken}&public=true`);
+
+    if (!response.ok) {
+      throw new Error('Request failed');
+    }
+
+    const data = await response.json();
+    // Process the data as needed
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Call the fetchData function after the page has fully loaded
+document.addEventListener('DOMContentLoaded', fetchData);
