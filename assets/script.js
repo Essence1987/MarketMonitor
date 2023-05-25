c1422129d372406d6937e25097ef39f022578ea3
Kyung Kwon <kyungkwon01@gmail.com>
2023-05-24 20:48:08 -0600

    adding base files

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
-var key = config.marketaux;
+const searchButtonEl = document.querySelector("#search_button");
+const recentNewsFeedEl = document.querySelector("#main_recent-news");
+const current_graph = document.querySelector("#current_graph");
+const searchNewsEl = document.querySelector("#search_news");
+
+searchButtonEl.addEventListener("click", function () {
+  recentNewsFeedEl.classList.add("hidden"),
+    current_graph.classList.add("hidden"),
+    searchNewsEl.classList.remove("hidden");
+});
