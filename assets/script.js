4424ba4f3b9604ce85061e625ff1b746b99c7dd0
Kyung Kwon <kyungkwon01@gmail.com>
2023-05-25 19:01:33 -0600

    add renderSearchedNews function

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,26 @@
-
-
 //GET https://api.marketaux.com/v1/news/all HTTP/1.1
 
 var requestOptions = {
-    method: 'GET'
+  method: "GET",
 };
 
 var params = {
-    api_token: 'JuJOIG1hHbpKquZiBFJtJ4OCTTKIavGBRQyzQ4bN',
-    symbols: 'msft,fb',
-    limit: '3'
+  api_token: "JuJOIG1hHbpKquZiBFJtJ4OCTTKIavGBRQyzQ4bN",
+  symbols: "msft,fb",
+  limit: "3",
 };
 
 var esc = encodeURIComponent;
 var query = Object.keys(params)
-    .map(function(k) {return esc(k) + '=' + esc(params[k]);})
-    .join('&');
+  .map(function (k) {
+    return esc(k) + "=" + esc(params[k]);
+  })
+  .join("&");
 
 fetch("https://api.marketaux.com/v1/news/all?" + query, requestOptions)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log('error', error));
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.log("error", error));
 
 const searchButtonEl = document.querySelector("#search_button");
 const recentNewsFeedEl = document.querySelector("#main_recent-news");
@@ -28,7 +28,15 @@ const current_graph = document.querySelector("#current_graph");
 const searchNewsEl = document.querySelector("#search_news");
 
 searchButtonEl.addEventListener("click", function () {
-  recentNewsFeedEl.classList.add("hidden"),
-    current_graph.classList.add("hidden"),
-    searchNewsEl.classList.remove("hidden");
+  recentNewsFeedEl.classList.add("d-none"),
+    current_graph.classList.add("d-none"),
+    searchNewsEl.classList.remove("d-none");
 });
+
+function renderSearchedNews() {
+  const searchedNewsEl = document.querySelectorAll('.card')
+  for (i = 0; i < forecastEls.length; i++) {
+    searchedNewsEl[i].innerHTML = '';
+
+  }
+}
