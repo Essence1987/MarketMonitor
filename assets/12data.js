102d47fe1bf76d805c80d96d9bfe93e93cdb14ee
Essence1987 <hwmelander@gmail.com>
2023-05-25 18:27:33 -0700

    fixed typo and added function to call fetchData when document finishes loading for testing purposes.

--- assets/12data.js
+++ assets/12data.js
@@ -1,18 +1,16 @@
+function fetchData() {
 // 12 Data API Key 
 const apikey = 'b3653483401543c5a9c34983f41185fb';
-
 // This is where the user input search will go. For now one is entered for trial run
 const symbols = 'AAPL';
-
 // Desired interval for the data. 
 const interval = '1min';
-
 // API URL
 const apiUrl= `https://api.twelvedata.com/time_series?symbol=${symbols}&interval=${interval}&apikey=${apiKey}`;
 
 // The API Request
 fetch(apiUrl)
-    .then(responce => Response.json())
+    .then(response => response.json())
     .then(data => {
         // Displays the retrieved data in console
         console.log(data);
@@ -21,3 +19,7 @@ fetch(apiUrl)
         // Displays any errors that occur during the API request in the console
         console.error('Error:', error);
     });
+}
+
+// PlaceHolder, will currently call the fetchData function when the document is ready
+document.addEventListener('DOMContentLoaded', fetchData);
