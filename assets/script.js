306377c51e249e9cbdcb976f23c1121ab35c3116
Lisha Kuinkel <ltkuinkel@gmail.com>
2023-05-31 18:28:26 -0600

    added article url as Read more

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ const current_graph = document.querySelector("#current_graph");
 const searchNewsEl = document.querySelector("#search_news");
 const searchInput = document.getElementById("search_input");
 const recommendNewsEl = document.getElementById("recommended_news");
+const divCard = document.querySelector(".col");
 
 recommendNewsEl.style.display = "none";
 
@@ -16,12 +17,11 @@ searchButtonEl.addEventListener("click", function () {
     fetchStockNewsApi()
 });
 
-
 function fetchStockNewsApi(){
 
 var inputValue = searchInput.textContent;
 
-fetch("https://api.marketaux.com/v1/news/all?symbols=" + inputValue + "&filter_entities=true&language=en&api_token=" + api_token)
+fetch("https://api.marketaux.com/v1/news/all?symbols=" + inputValue + "&filter_entities=true&limit=3&language=en&api_token=" + api_token)
     .then (function (response){
         return response.json();
     })
@@ -32,16 +32,25 @@ fetch("https://api.marketaux.com/v1/news/all?symbols=" + inputValue + "&filter_e
         var articleDescription = data.data[i].description;
         var articleImg = data.data[i].image_url;
         var articleTitle = data.data[i].title;
+        var articleUrl = data.data[i].url;
 
         console.log(articleTitle);
         console.log(articleDescription);
         console.log(articleImg);     
-        
+        console.log(articleUrl);
+
         document.querySelector(".card-title" + (i+1)).innerHTML = articleTitle;
         document.querySelector(".card-text" + (i+1)).innerHTML = articleDescription;
         document.querySelector(".card-img-top" + (i+1)).src = articleImg;
+        document.querySelector(".card-url" + (i+1)).href = articleUrl;
+        
         
         }
     })
 }
 
+
+
+function fetchSimilarStockNewsApi(){
+    fetch("")
+}
